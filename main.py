bd7dcd944ef0cd89c477b3e3b8d86a134d3ba057
Ranna <jran0323@gmail.com>
2023-08-21 11:40:57 +0900

    #JSONReader
    - data 폴더에 kaggle Data(Food Recipes) 추가
    ****************************************
    https://www.kaggle.com/datasets/crispen5gar/recipes3k?select=recipes.json
    ****************************************
    - llama-index JSONReader로 해당 데이터 로드하기

--- main.py
+++ main.py
@@ -6,3 +6,12 @@ load_dotenv()
 
 # 환경 변수 사용
 OPEN_AI_KEY = os.getenv("OPENAI_API_KEY")
+
+# 문서 파일 로드하기(llama-index json loader)
+from llama_index import download_loader
+from pathlib import Path
+
+JSONReader = download_loader("JSONReader")
+loader = JSONReader()
+documents = loader.load_data(Path('./data/recipes.json'))
+print(f"Loaded {len(documents)} docs")
