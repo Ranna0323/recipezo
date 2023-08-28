361682230a23f010526661311288882b5f65e1ac
Ranna <jran0323@gmail.com>
2023-08-28 09:58:46 +0900

    #chromadb
    - add chromadb

--- recipezo_app/views.py
+++ recipezo_app/views.py
@@ -17,18 +17,19 @@ from llama_index import download_loader
 from pathlib import Path
 
 # 인덱스 구성하기
-from llama_index import LLMPredictor, GPTVectorStoreIndex, PromptHelper, ServiceContext, StorageContext, load_index_from_storage
+from llama_index import LLMPredictor, PromptHelper, ServiceContext
 from langchain.chat_models import ChatOpenAI
+
 
 index = None
 
 # 인덱스 초기화
 def initialise_index():
     global index
-    # JSONReader = download_loader("JSONReader")
-    # loader = JSONReader()
-    # documents = loader.load_data(Path('./data/recipes.json'))
-    # print(f"Loaded {len(documents)} docs")
+    JSONReader = download_loader("JSONReader")
+    loader = JSONReader()
+    documents = loader.load_data(Path('./data/recipes.json'))
+    print(f"Loaded {len(documents)} docs")
 
     llm_predictor = LLMPredictor(llm=ChatOpenAI(temperature=0, model_name='gpt-3.5-turbo'))
 
@@ -39,19 +40,37 @@ def initialise_index():
     max_chuck_overlap = 0.1
     promt_helper = PromptHelper(max_input_size, num_output, max_chuck_overlap)
 
-    service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=promt_helper)
+    # service_context = ServiceContext.from_defaults(llm_predictor=llm_predictor, prompt_helper=promt_helper)
 
-    # 아래 두 줄은 한번 vectorstore(.storage)폴더에 저장 되고 나면 다시 실행할 필요 X => 추가 비용 발생 문제 생길 수 있음
-    # 단순 벡터 인덱스 생성
-    # index = GPTVectorStoreIndex.from_documents(documents, service_context=service_context)
-    #
-    # #기본적으로, 데이터는 메모리에 저장됩니다. 디스크에 저장하려면 (./storage)등과 같이 지정 해줘야 함
-    # index.storage_context.persist("./storage")
+    # import
+    from llama_index import VectorStoreIndex
+    # chroma db
+    from llama_index.vector_stores import ChromaVectorStore
+    from llama_index.embeddings import OpenAIEmbedding
 
-    # 저장 컨텍스트 다시 구성
-    storage_context = StorageContext.from_defaults(persist_dir='./storage')
-    # 인덱스 로드
-    index = load_index_from_storage(storage_context)
+    import chromadb
+
+    # Embedding
+    embed_model = OpenAIEmbedding()
+
+    # save to disk
+    # db = chromadb.PersistentClient(path="./chroma_db")
+    # chroma_collection = db.get_or_create_collection("recipe_data")
+    # vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    # storage_context = StorageContext.from_defaults(vector_store=vector_store)
+    service_context = ServiceContext.from_defaults(embed_model=embed_model, llm_predictor=llm_predictor, prompt_helper=promt_helper)
+    # index = VectorStoreIndex.from_documents(
+    #     documents, storage_context=storage_context, service_context=service_context
+    # )
+
+    # load from disk
+    db2 = chromadb.PersistentClient(path="./chroma_db")
+    chroma_collection = db2.get_or_create_collection("recipe_data")
+    vector_store = ChromaVectorStore(chroma_collection=chroma_collection)
+    index = VectorStoreIndex.from_vector_store(
+        vector_store,
+        service_context=service_context,
+    )
 
 initialise_index()
 
